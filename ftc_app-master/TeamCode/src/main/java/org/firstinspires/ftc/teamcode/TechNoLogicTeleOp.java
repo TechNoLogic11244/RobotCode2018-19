75ada7db3fe10b21d39112428ac9b0173d130dbb
jiblypuff <jiblypuff12@gmail.com>
2018-10-14 16:01:03 -0400

    initial commit

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechNoLogicTeleOp.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * This OpMode ramps a single motor speed up and down repeatedly until Stop is pressed.
+ * The code is structured as a LinearOpMode
+ *
+ * This code assumes a DC motor configured with the name "left_drive" as is found on a pushbot.
+ *
+ * INCREMENT sets how much to increase/decrease the power each cycle
+ * CYCLE_MS sets the update period.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+@TeleOp(name = "Concept: Ramp Motor Speed", group = "Concept")
+//@Disabled
+public class TechNoLogicTeleOp extends OpMode{
+
+    // Define class members
+    DcMotor leftFront;
+    DcMotor leftRear;
+    DcMotor rightFront;
+    DcMotor rightRear;
+
+    Servo leftClaw;
+    Servo rightClaw;
+
+    // don't put llamas as the variable name :)
+    final double LEFT_OPEN = 0.3;
+    final double RIGHT_OPEN = 0.7;
+    final double LEFT_CLOSE = 0.5;
+    final double RIGHT_CLOSE = 0.5;
+
+    @Override
+    public void init() {
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+
+        leftClaw = hardwareMap.servo.get("leftClaw");
+        rightClaw = hardwareMap.servo.get("rightClaw");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftRear.setDirection(DcMotor.Direction.REVERSE);
+
+        // Can use this line, but can also use opposites (set one to 0, the other to 1)
+        //rightClaw.setDirection(Servo.Direction.REVERSE);
+
+        // Wait for the start button
+        telemetry.addData(">", "Press Start" );
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+
+        // Variables for joystick values
+        double leftDrives = -gamepad1.left_stick_y;
+        double rightDrives = -gamepad1.right_stick_y;
+
+        // Set motor powers to joystick values
+        leftFront.setPower(leftDrives);
+        rightFront.setPower(rightDrives);
+        leftRear.setPower(leftDrives);
+        rightRear.setPower(rightDrives);
+
+        if (gamepad2.a){
+            leftClaw.setPosition(LEFT_OPEN);
+            rightClaw.setPosition(RIGHT_OPEN);
+        } else if (gamepad2.b){
+            leftClaw.setPosition(LEFT_CLOSE);
+            rightClaw.setPosition(RIGHT_CLOSE);
+        }
+
+        // Telemetry output for driver assistance
+        telemetry.addData(">", "Press Stop to end program." );
+        telemetry.update();
+    }
+}
+
