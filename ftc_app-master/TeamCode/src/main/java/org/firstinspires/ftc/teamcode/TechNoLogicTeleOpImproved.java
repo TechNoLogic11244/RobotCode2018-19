ff73cb90721bbb387bd9c21df31c3be2605769ce
jiblypuff <jiblypuff12@gmail.com>
2018-10-24 20:09:37 -0400

    added arm code to teleop

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechNoLogicTeleOpImproved.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TechNoLogicTeleOpImproved.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.sun.tools.javac.tree.DCTree;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * This OpMode ramps a single motor speed up and down repeatedly until Stop is pressed.
@@ -25,17 +25,24 @@ public class TechNoLogicTeleOpImproved extends OpMode{
     // Define class members
     DcMotor leftRear;
     DcMotor rightRear;
-    DcMotor topArm;
-    DcMotor bottomArm;
+    DcMotor extensionMotor;
+    DcMotor rotationMotor;
 
     Servo leftClaw;
     Servo rightClaw;
 
-    // put llamas as the variable name :)
+    // servo constants (TBD, find accurate values through testing)
     final double LEFT_OPEN = 0.3;
     final double RIGHT_OPEN = 0.7;
     final double LEFT_CLOSE = 0.5;
     final double RIGHT_CLOSE = 0.5;
+
+    // arm variables (TBD, find accurate values through testing)
+    final double EXTENSION_POWER = 0.5;
+    final int MAX_EXTENSION      = 5000;
+    final int MIN_EXTENSION      = 100;
+    final int MIN_ROTATION       = 100;
+    final int MAX_ROTATION       = 5000;
 
     @Override
     public void init() {
@@ -43,13 +50,18 @@ public class TechNoLogicTeleOpImproved extends OpMode{
         leftRear   = hardwareMap.get(DcMotor.class, "leftRear");
         rightRear  = hardwareMap.get(DcMotor.class, "rightRear");
 
-        topArm = hardwareMap.get(DcMotor.class, "topArm");
-        bottomArm = hardwareMap.get(DcMotor.class, "bottomArm");
+        extensionMotor = hardwareMap.get(DcMotor.class, "extensionMotor");
+        rotationMotor = hardwareMap.get(DcMotor.class, "rotationMotor");
 
         leftClaw  = hardwareMap.servo.get("leftClaw");
         rightClaw = hardwareMap.servo.get("rightClaw");
 
         leftRear.setDirection(DcMotor.Direction.REVERSE);
+
+        extensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rotationMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        extensionMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rotationMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Can use this line, but can also use opposites (set one to 0, the other to 1)
         //rightClaw.setDirection(Servo.Direction.REVERSE);
@@ -66,18 +78,22 @@ public class TechNoLogicTeleOpImproved extends OpMode{
         double leftDrives  = -gamepad1.left_stick_y;
         double rightDrives = -gamepad1.right_stick_y;
 
-        double linearSlide = -gamepad2.left_stick_y;
         double bottomRotation = -gamepad2.right_stick_y;
-        
-        Range.clip(leftDrive, -0.75, 0.75);
-        Range.clip(rightDrive, -0.75, 0.75);
+
+        Range.clip(leftDrives, -0.75, 0.75);
+        Range.clip(rightDrives, -0.75, 0.75);
+
+        Range.clip(bottomRotation, -0.4, 0.4);
         
         // Set motor powers to joystick values
         leftRear.setPower(leftDrives);
         rightRear.setPower(rightDrives);
 
-        topArm.setPower(linearSlide);
-        bottomArm.setPower(bottomRotation);
+        if (rotationMotor.getCurrentPosition() <= MAX_ROTATION &&
+                rotationMotor.getCurrentPosition() >= MIN_ROTATION) {
+            rotationMotor.setPower(bottomRotation);
+        } else
+            extensionMotor.setPower(0.0);
 
         if (gamepad2.a){
             leftClaw.setPosition(LEFT_OPEN);
@@ -86,6 +102,17 @@ public class TechNoLogicTeleOpImproved extends OpMode{
             leftClaw.setPosition(LEFT_CLOSE);
             rightClaw.setPosition(RIGHT_CLOSE);
         }
+
+        if (extensionMotor.getCurrentPosition() <= MAX_EXTENSION &&
+                extensionMotor.getCurrentPosition() >= MIN_EXTENSION) {
+            if (gamepad2.dpad_up)
+                extensionMotor.setPower(EXTENSION_POWER);
+            else if (gamepad2.dpad_down)
+                extensionMotor.setPower(-EXTENSION_POWER);
+            else
+                extensionMotor.setPower(0.0);
+        } else
+            extensionMotor.setPower(0.0);
 
         // Telemetry output for driver assistance
         telemetry.addData(">", "Press Stop to end program." );
